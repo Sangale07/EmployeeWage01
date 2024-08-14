c3c52492451479b32446d63169638a0d98bac4f3
rushi <rushikeshsangale57@gmail.com>
2024-08-14 17:14:21 +0530

    Welcome to Employee Wage Program

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation program");
+
+
+    }
+}
+
+
+
+
+
+
+
+
