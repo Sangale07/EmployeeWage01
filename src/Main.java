d8f4d47291bb5a99e8598a04660bd53f78c592cf
rushi <rushikeshsangale57@gmail.com>
2024-08-22 16:20:11 +0530

    DailyEmpWage is Added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int isPresent = (int) (Math.random() * 2); // 0 or 1
+        int isPresent = (int) (Math.random() * 2);
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
         if (isPresent == 1) {
+            int dailyWage = wagePerHour * fullDayHour;
             System.out.println("Employee is Present");
+            System.out.println("Daily Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Wage: 0");
         }
     }
 }
