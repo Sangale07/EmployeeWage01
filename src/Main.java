93beca2956e566dd1566d7a4f61769f62238705d
rushi <rushikeshsangale57@gmail.com>
2024-08-22 16:45:30 +0530

    Calculate Wages for month

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int empType = (int) (Math.random() * 3);
         int wagePerHour = 20;
         int fullDayHour = 8;
         int partTimeHour = 4;
-        int dailyWage = 0;
+        int totalWage = 0;
+        int workingDays = 20;
 
-        switch (empType) {
-            case 1:
-                dailyWage = wagePerHour * fullDayHour;
-                System.out.println("Employee is Present (Full-time)");
-                break;
-            case 2:
-                dailyWage = wagePerHour * partTimeHour;
-                System.out.println("Employee is Present (Part-time)");
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                break;
+        for (int day = 1; day <= workingDays; day++) {
+            int empType = (int) (Math.random() * 3);
+            int dailyWage = 0;
+
+            switch (empType) {
+                case 1:
+                    dailyWage = wagePerHour * fullDayHour;
+                    break;
+                case 2:
+                    dailyWage = wagePerHour * partTimeHour;
+                    break;
+                default:
+                    dailyWage = 0;
+                    break;
+            }
+            totalWage += dailyWage;
+            System.out.println("Day " + day + ": Daily Wage: " + dailyWage);
         }
-        System.out.println("Daily Wage: " + dailyWage);
+        System.out.println("Total Wage for the Month: " + totalWage);
     }
-
 }
