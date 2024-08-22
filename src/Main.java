1a9cdc24a7de9eda20fb83a28938a457a0e7a590
rushi <rushikeshsangale57@gmail.com>
2024-08-22 16:07:39 +0530

    Employee Attendance program is added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int isPresent = (int) (Math.random() * 2); // 0 or 1
+        if (isPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
